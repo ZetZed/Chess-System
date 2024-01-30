80610a5c332e3bea332964247ade6fb6bb14869e
Zet Zed <rodhaste@hotmail.com>
2024-01-30 13:33:22 -0400

    Starting to implement Board and Piece

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,10 +1,9 @@
 package Application;
 
-import BoardGame.Position;
+import BoardGame.Board;
 
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        Board board = new Board(8, 8);
     }
 }

943a4d71f8d660f5678c3962e7720f85e463d715
Zet Zed <rodhaste@hotmail.com>
2024-01-30 13:25:37 -0400

    First class: Position

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,7 +1,10 @@
 package Application;
 
+import BoardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
